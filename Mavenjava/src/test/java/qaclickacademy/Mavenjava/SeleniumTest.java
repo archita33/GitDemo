51c8a99e3db7796300f9c364297ee68d2d7da68c
archita33 <architajain33@gmail.com>
2022-01-11 03:02:09 +0530

    firstcommit

---
+++ Mavenjava/src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,12 @@
+package qaclickacademy.Mavenjava;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void Browserautomation()
+	{
+		System.out.println("Hello, I am in Maven Project");
+	}
+	
+}
